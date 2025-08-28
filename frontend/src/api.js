d8d984cb207dc2913a7bb64e9b4fe784d39b513f
alamin20cse <md.alamin165086@gmail.com>
2025-08-29 00:41:43 +0600

    done

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import { ACCESS_TOKEN,REFRESH_TOKEN } from "./constants"
 
 const api=axios.create({
-    baseURL:"http://127.0.0.1:8000/api"
+    baseURL:"https://review-system-wls0.onrender.com/api"
 })
 
 api.interceptors.request.use(
