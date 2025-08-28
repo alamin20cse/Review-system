07447bcb3a3f1e7d8e3a646757fcd85202026caf
alamin20cse <md.alamin165086@gmail.com>
2025-08-29 00:55:58 +0600

    phot url in url prattern correction

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -39,11 +39,12 @@ urlpatterns = [
 
 
 
+# Serve media files (only for local/dev)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-# React SPA fallback route
+# SPA fallback route (React)
 urlpatterns += [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
